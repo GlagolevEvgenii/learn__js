68de9f6954d9426a7a9c953b8cabca313aca6898
Evgenii <e.glagolev@artjoker.ua>
2020-06-15 17:19:50 +0300

    start extra hw 4

---
+++ js/dop.js
@@ -0,0 +1,14 @@
+function  pushArray() {
+    let arr = [];
+    let question = +prompt("сколько массивов включить во внутрь arr" ,"");
+    for(let i = 0; i < question; i++){
+        let number = Math.random();
+        arr[i] = [];
+        for (let k = 0; k < question; k++){
+
+            console.log(arr[k]);
+        }
+
+    }
+}
+pushArray();
